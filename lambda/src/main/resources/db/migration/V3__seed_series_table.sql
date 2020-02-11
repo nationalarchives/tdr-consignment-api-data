9c61520b2fd16bc96b7c190991afb4102b20217c
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-02-11 13:12:40 +0000

    Add seed data to Body and Series tables

---
+++ lambda/src/main/resources/db/migration/V3__seed_series_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO Series (bodyid, code, name, description) VALUES
+  (1, 'MOCK1 123', 'MOCK1 123', 'This is a mocked series from the MOCK1 body'),
+  (2, 'MOCK2 234', 'MOCK2 234', 'This is a mocked series from the MOCK2 body'),
+  (2, 'MOCK2 345', 'MOCK2 345', 'This is a mocked series from the MOCK2 body'),
+  (3, 'MOCK3 456', 'MOCK3 456', 'This is a mocked series from the MOCK3 body');
