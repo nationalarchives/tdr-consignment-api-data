85b24e2c07a27e668da0a81a9508f4b09fa13719
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-08-24 11:32:48 +0100

    Fix migration to make antivirus fields non-null
    The migration failed in integration because some of the rows had null
    antivirus data. This is probably because of test data, so it should be
    safe to insert placeholder values into those fields.

--- lambda/src/main/resources/db/migration/V9__make_antivirus_fields_mandatory.sql
+++ lambda/src/main/resources/db/migration/V9__make_antivirus_fields_mandatory.sql
@@ -1,3 +1,19 @@
+-- Add placeholders for missing data. This is safe because values should only
+-- be null for test data that was added manually.
+UPDATE "AVMetadata"
+  SET "Software" = 'PLACEHOLDER_SOFTWARE'
+  WHERE "Software" IS NULL;
+UPDATE "AVMetadata"
+  SET "SoftwareVersion" = 'PLACEHOLDER_SOFTWARE_VERSION'
+  WHERE "SoftwareVersion" IS NULL;
+UPDATE "AVMetadata"
+  SET "DatabaseVersion" = 'PLACEHOLDER_DATABASE_VERSION'
+  WHERE "DatabaseVersion" IS NULL;
+UPDATE "AVMetadata"
+  SET "Result" = 'PLACEHOLDER_RESULT'
+  WHERE "Result" IS NULL;
+
+-- Make fields non-null
 ALTER TABLE "AVMetadata"
   ALTER COLUMN "Software" SET NOT NULL,
   ALTER COLUMN "SoftwareVersion" SET NOT NULL,
