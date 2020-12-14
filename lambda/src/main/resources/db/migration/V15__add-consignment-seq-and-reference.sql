e1b13d8d3daca2232021e6d22097048a3df053a4
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-12-14 11:36:49 +0000

    Add consignment reference column and sequence with assocciated column

--- lambda/src/main/resources/db/migration/V15__add-consignment-seq-and-reference.sql
+++ lambda/src/main/resources/db/migration/V15__add-consignment-seq-and-reference.sql
@@ -1,10 +1,15 @@
 -- Create sequence that will be the consignment sequence ID used to encode b25 section of consignment reference
-CREATE SEQUENCE "ConsignmentSequence"
+CREATE SEQUENCE "ConsignmentSequenceID"
 NO CYCLE
 START WITH 1
 INCREMENT BY 1;
 
 -- Alter Consignment table to add column to store sequence ID and another for full consignment reference
 ALTER TABLE "Consignment"
-ADD COLUMN "ConsignmentSequence" bigint,
-ADD COLUMN "ConsignmentReference" text
+ADD COLUMN "ConsignmentSequence" bigint DEFAULT nextval('"ConsignmentSequenceID"'),
+ADD COLUMN "ConsignmentReference" text,
+ADD UNIQUE ("ConsignmentSequence", "ConsignmentReference");
+
+-- Alter consignment table to create consignment reference for existing Consignment rows
+UPDATE "Consignment"
+SET "ConsignmentReference"='TDR-2020-OLD-' || "ConsignmentSequence";
