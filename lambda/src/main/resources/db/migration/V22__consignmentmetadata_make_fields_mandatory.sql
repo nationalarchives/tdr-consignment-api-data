e9980cfe53f01ef8756fb3bde234366092fb84ee
TomJKing <tom.king@nationalarchives.gov.uk>
2021-01-11 10:34:14 +0000

    Renumber and update field name

--- lambda/src/main/resources/db/migration/V22__consignmentmetadata_make_fields_mandatory.sql
+++ lambda/src/main/resources/db/migration/V22__consignmentmetadata_make_fields_mandatory.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "ConsignmentMetadata"
     ALTER COLUMN "ConsignmentId" SET NOT NULL,
-    ALTER COLUMN "PropertyId" SET NOT NULL,
+    ALTER COLUMN "PropertyName" SET NOT NULL,
     ALTER COLUMN "Value" SET NOT NULL;
