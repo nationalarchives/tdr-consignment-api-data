4b248a679a05161a7210fdeeb1e21fb27e34c6d8
TomJKing <tom.king@nationalarchives.gov.uk>
2021-01-12 07:16:11 +0000

    Ensure no nulls in 'Value' field before making non-nullable

--- lambda/src/main/resources/db/migration/V22__consignmentmetadata_make_fields_mandatory.sql
+++ lambda/src/main/resources/db/migration/V22__consignmentmetadata_make_fields_mandatory.sql
@@ -1,3 +1,7 @@
+UPDATE "ConsignmentMetadata"
+    SET "Value" = 'false'
+    WHERE "Value" IS NULL;
+
 ALTER TABLE "ConsignmentMetadata"
     ALTER COLUMN "ConsignmentId" SET NOT NULL,
     ALTER COLUMN "PropertyName" SET NOT NULL,
