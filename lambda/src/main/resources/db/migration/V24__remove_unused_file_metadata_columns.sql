93340f28df9f30ee11947cb996e521c8e40a7917
sam <sam.palmer@nationalarchives.gov.uk>
2021-01-13 14:41:27 +0000

    Migrate data and drop the old table

--- lambda/src/main/resources/db/migration/V24__remove_unused_file_metadata_columns.sql
+++ lambda/src/main/resources/db/migration/V24__remove_unused_file_metadata_columns.sql
@@ -2,3 +2,21 @@ UPDATE "FileMetadata" fm SET "PropertyName" = fp."Name" FROM "FileProperty" fp W
 ALTER TABLE "FileMetadata" DROP COLUMN "PropertyId";
 ALTER TABLE "FileProperty" DROP COLUMN "PropertyId";
 ALTER TABLE "FileMetadata" ALTER COLUMN "PropertyName" SET NOT NULL;
+
+INSERT INTO "FileMetadata"
+SELECT uuid_generate_v4(), f."FileId", "OriginalPath", cfm."Datetime", "UserId", 'ClientSideOriginalFilepath' 
+FROM "ClientFileMetadata" cfm JOIN "File" f on f."FileId" = cfm."FileId";
+
+INSERT INTO "FileMetadata"
+SELECT uuid_generate_v4(), f."FileId", "LastModified", cfm."Datetime", "UserId", 'ClientSideFileLastModifiedDate' 
+FROM "ClientFileMetadata" cfm JOIN "File" f on f."FileId" = cfm."FileId";
+
+INSERT INTO "FileMetadata"
+SELECT uuid_generate_v4(), f."FileId", "Filesize", cfm."Datetime", "UserId", 'ClientSideFileSize' 
+FROM "ClientFileMetadata" cfm JOIN "File" f on f."FileId" = cfm."FileId";
+
+INSERT INTO "FileMetadata"
+SELECT uuid_generate_v4(), f."FileId", "Checksum", cfm."Datetime", "UserId", 'SHA256ClientSideChecksum' 
+FROM "ClientFileMetadata" cfm JOIN "File" f on f."FileId" = cfm."FileId";
+
+DROP TABLE "ClientFileMetadata";
