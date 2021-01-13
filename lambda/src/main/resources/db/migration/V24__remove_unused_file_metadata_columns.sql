05b4bec61037df887cd02438fc27e8567b0b830f
sam <sam.palmer@nationalarchives.gov.uk>
2021-01-13 14:08:01 +0000

    Set PropertyName as not null.
    Now PropertyId isn't used, this can't be null.

--- lambda/src/main/resources/db/migration/V24__remove_unused_file_metadata_columns.sql
+++ lambda/src/main/resources/db/migration/V24__remove_unused_file_metadata_columns.sql
@@ -1,3 +1,4 @@
 UPDATE "FileMetadata" fm SET "PropertyName" = fp."Name" FROM "FileProperty" fp WHERE fm."PropertyId" = fp."PropertyId";
 ALTER TABLE "FileMetadata" DROP COLUMN "PropertyId";
 ALTER TABLE "FileProperty" DROP COLUMN "PropertyId";
+ALTER TABLE "FileMetadata" ALTER COLUMN "PropertyName" SET NOT NULL;
