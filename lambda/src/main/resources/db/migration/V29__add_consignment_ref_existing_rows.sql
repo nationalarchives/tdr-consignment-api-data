630d9fa86a79ec56c0b13327c64f47d58f77a692
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2021-02-01 12:10:37 +0000

    Add comments to explain why existing consignment refs will be different

--- lambda/src/main/resources/db/migration/V29__add_consignment_ref_existing_rows.sql
+++ lambda/src/main/resources/db/migration/V29__add_consignment_ref_existing_rows.sql
@@ -1,4 +1,7 @@
--- Update consignment table to create consignment reference for existing Consignment rows
+-- Update consignment table to create reference for existing Consignment rows
+-- For this we are not using the encoded consignment reference as it was too difficult to encode the reference within a database migration.
+-- This change will only affect existing data in intg and staging (all test data since TDR has not been released).
+-- New consignments will have the properly encoded consignment reference format agreed upon by TDR developers.
 UPDATE "Consignment"
 SET "ConsignmentReference"=CONCAT('TDR-2020-OLD-', "ConsignmentSequence");
 
