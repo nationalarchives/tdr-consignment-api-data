8158b14e140048b9aeb90e53d77795c60c8274c6
Loki Holbourn <loki.holbourn@nationalarchives.gov.uk>
2021-03-16 09:29:34 +0000

    Remove on update and rename "DateTime" column

--- lambda/src/main/resources/db/migration/V31__add_consignment_status_table.sql
+++ lambda/src/main/resources/db/migration/V31__add_consignment_status_table.sql
@@ -4,11 +4,8 @@ CREATE TABLE "ConsignmentStatus"
     "ConsignmentId" uuid NOT NULL,
     "StatusType" text NOT NULL,
     "Value" text NOT NULL,
-    "DateTime" timestamp with time zone not null,
+    "CreatedDatetime" timestamp with time zone not null,
     CONSTRAINT "ConsignmentStatus_pkey" PRIMARY KEY ("ConsignmentStatusId"),
     CONSTRAINT "ConsignmentStatus_Consignment_fkey" FOREIGN KEY ("ConsignmentId")
     REFERENCES "Consignment" ("ConsignmentId") MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID
 )
