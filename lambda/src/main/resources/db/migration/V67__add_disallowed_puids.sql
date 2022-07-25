d1b9358ab482c1f83a896530b4cbdbe95f6dd3c3
TomJKing <tom.king@nationalarchives.gov.uk>
2022-07-25 10:31:39 +0100

    Fix description spacing

--- lambda/src/main/resources/db/migration/V67__add_disallowed_puids.sql
+++ lambda/src/main/resources/db/migration/V67__add_disallowed_puids.sql
@@ -7,8 +7,8 @@ VALUES
     ('x-fmt/409','MS-DOS Executable','Executable', false),
     ('x-fmt/410','Windows New Executable','Executable', false),
     ('x-fmt/411','Windows Portable Executable','Executable', false),
-    ('fmt/899','Windows Portable Executable	32 bit','Executable', false),
-    ('fmt/900','Windows Portable Executable	64 bit','Executable', false),
+    ('fmt/899','Windows Portable Executable 32 bit','Executable', false),
+    ('fmt/900','Windows Portable Executable 64 bit','Executable', false),
     ('fmt/682','Thumbs DB file','SystemFile', false),
     ('zeroByteFile', 'Identifier for empty file not in PRONOM', 'ZeroByteFile', false);
 
