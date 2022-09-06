e3b7cace0632e832e1558af9d4ed5a04088a822b
juliuswelby <111987081+juliuswelby@users.noreply.github.com>
2022-09-06 17:34:39 +0100

    Query to add four field descriptions

---
+++ lambda/src/main/resources/db/migration/V71__add_ui_element_descriptions_to_FileProperty_table.sql
@@ -0,0 +1,14 @@
+UPDATE public."FileProperty"
+SET "Description" = (CASE
+                         WHEN "Name" = 'FoiExemptionAsserted'
+                         THEN 'Date of the Advisory Council approval (or SIRO approval if appropriate)'
+
+                         WHEN "Name" = 'ClosureStartDate'
+                         THEN 'This has been defaulted to the last date modified. If this is not correct, amend the field below.'
+
+                         WHEN "Name" = 'ClosurePeriod'
+                         THEN 'Number of years the record is closed from the closure start date'
+
+                         WHEN "Name" = 'FoiExemptionCode'
+                         THEN 'Select the exemption code that applies'
+                     END);
