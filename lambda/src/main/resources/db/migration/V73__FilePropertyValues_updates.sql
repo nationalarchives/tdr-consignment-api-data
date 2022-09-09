e4528d2c4fe57af5858cd7348a3f0e2f3a88dd8b
juliuswelby <111987081+juliuswelby@users.noreply.github.com>
2022-09-09 09:59:18 +0100

    Script to remove unused optional field values

---
+++ lambda/src/main/resources/db/migration/V73__FilePropertyValues_updates.sql
@@ -0,0 +1,32 @@
+--From a comment 
+
+-- FilePropertyValues table
+
+-- ClosureType values
+    -- closed_for
+    -- open_on_transfer
+
+-- Once again these are DRI terms TDR could just use Open or Closed
+
+-- But we need to make sure closed_until is not something that TDR would consider. In which case TRE wouldn’t know what to change it to.
+
+-- Is TDR giving the user a chance to change these from default?.
+-- If not we could remove all but one for the time being:
+
+--  LegalStatus
+    -- Public Record(s)
+    -- Welsh Public Record(s)
+    -- Not Public Record(s)
+
+-- RightsCopyright
+    -- Crown Copyright
+    -- Non Crown Copyright
+
+
+--Assuming we will remove all but the current default value
+
+DELETE from public."FilePropertyValues"
+WHERE "PropertyName" = 'LegalStatus' 
+AND "PropertyValue" IN ('Welsh Public Record(s)', 'Not Public Record(s)', 'Non Crown Copyright')
+
+COMMIT;
