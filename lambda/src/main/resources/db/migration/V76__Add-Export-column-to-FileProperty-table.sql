e1a84cd7148791cff168d5bd9605468611fe8214
juliuswelby <julius.welby@nationalarchives.gov.uk>
2022-09-14 16:02:03 +0100

    Corrected typo in table name

--- lambda/src/main/resources/db/migration/V76__Add-Export-column-to-FileProperty-table.sql
+++ lambda/src/main/resources/db/migration/V76__Add-Export-column-to-FileProperty-table.sql
@@ -1,7 +1,7 @@
 -- Add Export column to FileProperty table
 -- A boolean, indicating if a property should be included in metadata downloads
 
-ALTER TABLE public."FileProperies"
+ALTER TABLE public."FileProperty"
 ADD COLUMN "Export" BOOLEAN NOT NULL DEFAULT FALSE;
 
 CREATE INDEX FileProperty_ExportBool ON public."FileProperty" ("Export");
