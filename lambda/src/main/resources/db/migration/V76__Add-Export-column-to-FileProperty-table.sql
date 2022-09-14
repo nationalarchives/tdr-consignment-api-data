d35c380b840112a31f964eb11a1c0f6afe21db41
juliuswelby <julius.welby@nationalarchives.gov.uk>
2022-09-14 16:48:15 +0100

    Added ExportOrdinal column, and renamed file

--- lambda/src/main/resources/db/migration/V76__Add-Export-column-to-FileProperty-table.sql
+++
@@ -1,9 +0,0 @@
--- Add Export column to FileProperty table
--- A boolean, indicating if a property should be included in metadata downloads
-
-ALTER TABLE public."FileProperty"
-ADD COLUMN "Export" BOOLEAN NOT NULL DEFAULT FALSE;
-
-CREATE INDEX FileProperty_ExportBool ON public."FileProperty" ("Export");
-
-COMMIT;
---
+++ lambda/src/main/resources/db/migration/V76__Add-export-related-columns-to-FileProperty-table.sql
@@ -0,0 +1,15 @@
+-- Add Export column to FileProperty table
+-- A boolean, indicating if a property should be included in metadata downloads
+-- Indexes added to speed up retrieval and sorting
+
+ALTER TABLE public."FileProperty"
+ADD COLUMN "ExportOrdinal" SMALLINT;
+
+ALTER TABLE public."FileProperty"
+ADD COLUMN "AllowExport" BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE INDEX FileProperty_ExportOrdinal ON public."FileProperty" ("ExportOrdinal");
+
+CREATE INDEX FileProperty_AllowExport ON public."FileProperty" ("AllowExport");
+
+COMMIT;
