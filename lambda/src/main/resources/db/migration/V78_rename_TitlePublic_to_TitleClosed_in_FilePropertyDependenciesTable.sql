dc8cd2dda57744fdf47589d98e3435f89aa41a6b
juliuswelby <julius.welby@nationalarchives.gov.uk>
2022-09-28 14:45:14 +0100

    Added new migration file

---
+++ lambda/src/main/resources/db/migration/V78_rename_TitlePublic_to_TitleClosed_in_FilePropertyDependenciesTable.sql
@@ -0,0 +1,10 @@
+-- In the FilePropertyDependencies table, change "PropertyName" values
+-- TitlePublic--> TitleClosed
+-- (There are currently two affected rows).
+
+UPDATE public."FilePropertyDependencies"
+SET "PropertyName" = 'TitleClosed'
+WHERE "PropertyName" = 'TitlePublic';
+
+COMMIT;
+-
