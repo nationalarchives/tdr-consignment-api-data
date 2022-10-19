788a623482491e822f2d97ce9892fc5b2858e76e
TomJKing <tom.king@nationalarchives.gov.uk>
2022-10-19 09:53:06 +0100

    Fix body name

--- lambda/src/main/resources/db/migration/V79__create_ons_body_and_series.sql
+++ lambda/src/main/resources/db/migration/V79__create_ons_body_and_series.sql
@@ -4,7 +4,7 @@ DO $$
     BEGIN
         -- Insert 'Office of National Statistics' body into the  Body Table
         INSERT INTO "Body" ("BodyId", "Name", "Description", "TdrCode") VALUES
-        (uuid_generate_v4(), 'Office of National Statistics', 'Office of National Statistics', 'TDR-ONS') RETURNING "BodyId" INTO bodyUuid;
+        (uuid_generate_v4(), 'Office for National Statistics', 'Office for National Statistics', 'TDR-ONS') RETURNING "BodyId" INTO bodyUuid;
 
         -- Use the returned bodyUuid value for the series insert
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
