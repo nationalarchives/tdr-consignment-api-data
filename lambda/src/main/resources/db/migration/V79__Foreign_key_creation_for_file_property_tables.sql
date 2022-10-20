e75872f31a3c6979e5e137974ccf102ac3023c0a
pashworth-tna <phil.ashworth@nationalarchives.gov.uk>
2022-10-20 16:05:14 +0100

    Removed constraints not required, corrected naming conventions and comments

--- lambda/src/main/resources/db/migration/V79__Foreign_key_creation_for_file_property_tables.sql
+++ lambda/src/main/resources/db/migration/V79__Foreign_key_creation_for_file_property_tables.sql
@@ -1,22 +1,14 @@
 -- Add three foreign key constraints
--- FileProperty.Name as fk to FilePropertyValues.PropertyName
--- FileProperty.Name as fk to FilePropertyDependencies.PropertyName
--- FilePropertyValues.PropertyName as fk to FilePropertyDependencies.PropertyName
+-- FilePropertyValues.PropertyName as fk to FileProperty.Name
+-- FilePropertyDependencies.PropertyName as fk to FileProperty.Name
 
 ALTER TABLE "FilePropertyValues" ADD CONSTRAINT "FilePropertyValues_FileProperty_PropertyName_fkey"
     FOREIGN KEY ("PropertyName") REFERENCES "FileProperty" ("Name")
         ON UPDATE NO ACTION
         ON DELETE NO ACTION;
 
-ALTER TABLE "FilePropertyDependencies" ADD CONSTRAINT "FilePropertyDependencies_PropertyName_FileProperty_fkey"
+ALTER TABLE "FilePropertyDependencies" ADD CONSTRAINT "FilePropertyDependencies_FileProperty_PropertyName_fkey"
     FOREIGN KEY ("PropertyName") REFERENCES "FileProperty" ("Name")
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION;
-
-ALTER TABLE "FilePropertyValues" ADD CONSTRAINT "Dependencies_unique" UNIQUE ("Dependencies");
-
-ALTER TABLE "FilePropertyDependencies" ADD CONSTRAINT "FilePropertyDependencies_FilePropertyValues_GroupId_fkey"
-    FOREIGN KEY ("GroupId") REFERENCES "FilePropertyValues" ("Dependencies")
         ON UPDATE NO ACTION
         ON DELETE NO ACTION;
 
