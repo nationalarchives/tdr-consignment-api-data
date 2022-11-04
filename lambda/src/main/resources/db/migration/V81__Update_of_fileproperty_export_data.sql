e649ad2988dce4a02d64fa4d2affa18d472ed80c
pashworth-tna <phil.ashworth@nationalarchives.gov.uk>
2022-11-04 09:34:05 +0000

    Adjusted ordinal numbering

--- lambda/src/main/resources/db/migration/V81__Update_of_fileproperty_export_data.sql
+++ lambda/src/main/resources/db/migration/V81__Update_of_fileproperty_export_data.sql
@@ -5,7 +5,7 @@
 INSERT INTO "FileProperty" 
 	("Name", "Description", "FullName", "PropertyType", "Datatype", "Editable", "MultiValue", "PropertyGroup", "ExportOrdinal", "AllowExport" ) 
 	VALUES
-        ('FileType', 'States if the type is a file or folder', 'File Type', 'System', 'text', false, false, 'MandatoryMetadata', 2 ,true ) ;
+        ('FileType', 'States if the type is a file or folder', 'File Type', 'System', 'text', false, false, 'MandatoryMetadata', 10 ,true ) ;
 
 -- Next we update all of the properties that will be used in the export csv with ordinals and set to true.
 -- we have put Filename as the first ordinal as it comes from the File table.
@@ -14,31 +14,31 @@ UPDATE "FileProperty" as p SET
 	"AllowExport" = "p1"."AllowExport",
 	"ExportOrdinal" = "p1"."ExportOrdinal"
 FROM (VALUES
-	(true, 1,'Filename'),
-	(true, 3, 'ClientSideFileSize'),
-	(true, 4, 'ClientSideOriginalFilepath'),
-	(true, 5, 'RightsCopyright'),
-	(true, 6, 'LegalStatus'),
-	(true, 7, 'HeldBy'),
-	(true, 8, 'ClientSideFileLastModifiedDate'),
-	(true, 9, 'ClosureType'),
-	(true, 10, 'ClosureStartDate'),
-	(true, 11, 'ClosurePeriod'),
-	(true, 12, 'FoiExemptionCode'),
-	(true, 13, 'FoiExemptionAsserted'),
-	(true, 14, 'TitleClosed'),
-	(true, 15, 'TitleAlternate'),
-	(true, 16, 'description'),
-	(true, 17, 'DescriptionPublic'),
-	(true, 18, 'DescriptionAlternate'),
-	(true, 19, 'Language'),
-	(true, 20, 'date_created'),
-	(true, 21, 'date_range'),
-	(true, 22, 'end_date'),
-	(true, 23, 'start_date'),
-	(true, 24, 'file_name_language'),
-	(true, 25, 'file_name_translation'),
-	(true, 26, 'file_name_translation_language')
+	(true, 5,'Filename'),
+	(true, 15, 'ClientSideFileSize'),
+	(true, 20, 'ClientSideOriginalFilepath'),
+	(true, 25, 'RightsCopyright'),
+	(true, 30, 'LegalStatus'),
+	(true, 35, 'HeldBy'),
+	(true, 40, 'ClientSideFileLastModifiedDate'),
+	(true, 45, 'ClosureType'),
+	(true, 50, 'ClosureStartDate'),
+	(true, 55, 'ClosurePeriod'),
+	(true, 60, 'FoiExemptionCode'),
+	(true, 65, 'FoiExemptionAsserted'),
+	(true, 70, 'TitleClosed'),
+	(true, 75, 'TitleAlternate'),
+	(true, 80, 'description'),
+	(true, 85, 'DescriptionPublic'),
+	(true, 90, 'DescriptionAlternate'),
+	(true, 95, 'Language'),
+	(true, 100, 'date_created'),
+	(true, 105, 'date_range'),
+	(true, 110, 'end_date'),
+	(true, 115, 'start_date'),
+	(true, 120, 'file_name_language'),
+	(true, 125, 'file_name_translation'),
+	(true, 130, 'file_name_translation_language')
 ) as "p1"("AllowExport", "ExportOrdinal", "Name")
 WHERE "p1"."Name" = "p"."Name"
 ;
