50e640ac0edb8437c19b952766f5b4e44c14e66a
pashworth-tna <phil.ashworth@nationalarchives.gov.uk>
2022-11-07 10:34:47 +0000

    added commit statement

--- lambda/src/main/resources/db/migration/V82__Indexes_for_reporting.sql
+++ lambda/src/main/resources/db/migration/V82__Indexes_for_reporting.sql
@@ -6,3 +6,5 @@ CREATE INDEX FFIDMetadata_FileId_FFIDMetadataId_idx ON "FFIDMetadata" ("FileId",
 CREATE INDEX FileStatus_FileId_StatusType_idx ON "FileStatus" ("FileId", "StatusType");
 CREATE INDEX Consignment_ConsignmentId_ConsignmentReference_idx ON "Consignment" ("ConsignmentId", "ConsignmentReference");
 CREATE INDEX File_ConsignmentId_FileType_FileId_idx ON "File" ("ConsignmentId","FileType","FileId");
+
+COMMIT;
