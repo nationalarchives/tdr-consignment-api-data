e1cedfca3c75dab1cf0c6552acf531a5e9162fd1
juliuswelby <julius.welby@nationalarchives.gov.uk>
2022-12-06 15:22:19 +0000

    Fixed typo in SQL.

--- lambda/src/main/resources/db/migration/V90__remove_duplicated_foi_exemption_codes.sql
+++ lambda/src/main/resources/db/migration/V90__remove_duplicated_foi_exemption_codes.sql
@@ -3,7 +3,7 @@
 
 DELETE FROM "FilePropertyValues"
 WHERE "PropertyName" = 'FoiExemptionCode'
-AND "PropertyValue" IN ('43(1)','43(2')
+AND "PropertyValue" IN ('43(1)','43(2)')
 AND "Ordinal" IN (34,35);
 
 COMMIT;
