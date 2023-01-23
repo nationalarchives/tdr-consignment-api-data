c2432fff9ceb3fea1f6060d9fcc11f8bb39a1537
thanhz <thanhz@users.noreply.github.com>
2023-01-23 16:49:37 +0000

    TDR-2599 Update/Insert DisplayProperties data (#246)

---
+++ lambda/src/main/resources/db/migration/V94__update_and_insert_displayproperties_data.sql
@@ -0,0 +1,14 @@
+-- Update DisplayProperties table
+
+UPDATE "DisplayProperties" SET "Value" = '3' WHERE "PropertyName" = 'DescriptionAlternate' and "Attribute" = 'Group';
+UPDATE "DisplayProperties" SET "Value" = '3' WHERE "PropertyName" = 'TitleAlternate' and "Attribute" = 'Group';
+UPDATE "DisplayProperties" SET "Value" = 'small text' WHERE "PropertyName" = 'DescriptionAlternate' and "Attribute" = 'ComponentType';
+
+-- Insert new row to DisplayProperties table
+
+INSERT INTO "DisplayProperties" ("PropertyName","Attribute","Value","AttributeType")
+VALUES
+    ('ClosurePeriod','ComponentType','small text','text'),
+    ('FoiExemptionCode','ComponentType','select','text');
+
+COMMIT;
