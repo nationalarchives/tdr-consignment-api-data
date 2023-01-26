83664d8b08416b7ec1039a760f2b448a13841827
TomJKing <tom.king@nationalarchives.gov.uk>
2023-01-26 14:53:14 +0000

    Fix syntax and selector logic

--- lambda/src/main/resources/db/migration/V96__additional_metadata_statuses_backfill.sql
+++ lambda/src/main/resources/db/migration/V96__additional_metadata_statuses_backfill.sql
@@ -3,27 +3,28 @@
 DO $$
 
     DECLARE
-        closureMetadataFileIds UUID[] = ARRAY(
-            SELECT DISTINCT "FileId" FROM "FileStatus" WHERE  "StatusType" != 'ClosureMetadata'
+        closureMetadataFileIds uuid[] = ARRAY(
+            SELECT DISTINCT "FileId" FROM "FileStatus" WHERE NOT EXISTS (SELECT * FROM "FileStatus" WHERE "StatusType" = 'ClosureMetadata')
         );
-        descriptiveMetadataFileIds UUID[] = ARRAY(
-            SELECT DISTINCT "FileId" FROM "FileStatus" WHERE  "StatusType" != 'DescriptiveMetadata'
+        descriptiveMetadataFileIds uuid[] = ARRAY(
+            SELECT DISTINCT "FileId" FROM "FileStatus" WHERE NOT EXISTS (SELECT * FROM "FileStatus" WHERE "StatusType" = 'DescriptiveMetadata')
         );
+
+        fileId uuid;
 
     BEGIN
-
         FOREACH fileId IN ARRAY closureMetadataFileIds
             LOOP
                 -- insert default 'ClosureMetadata' status 'NotEntered'
                 INSERT INTO "FileStatus" ("FileStatusId", "FileId", "StatusType", "Value", "CreatedDatetime") VALUES
-                    (uuid_generate_v4(), fileId, "ClosureMetadata", "NotEntered", LOCALTIMESTAMP);
+                    (uuid_generate_v4(), fileId, 'ClosureMetadata', 'NotEntered', LOCALTIMESTAMP);
             END LOOP;
 
         FOREACH fileId IN ARRAY descriptiveMetadataFileIds
             LOOP
                 -- insert default 'DescriptiveMetadata' status 'NotEntered'
                 INSERT INTO "FileStatus" ("FileStatusId", "FileId", "StatusType", "Value", "CreatedDatetime") VALUES
-                    (uuid_generate_v4(), fileId, "DescriptiveMetadata", "NotEntered", LOCALTIMESTAMP);
+                    (uuid_generate_v4(), fileId, 'DescriptiveMetadata', 'NotEntered', LOCALTIMESTAMP);
             END LOOP;
 
 END; $$;
