3c0bfa48f99b19340bcbb4e3d8f271c387585056
thanhz <thanhz@hotmail.com>
2023-02-01 14:50:54 +0000

    New FutureDateAllowed attribute

--- lambda/src/main/resources/db/migration/V98__update_displayproperties.sql
+++ lambda/src/main/resources/db/migration/V98__update_displayproperties.sql
@@ -1,7 +1,23 @@
--- Update the 'DescriptionAlternate' attribute to large text
+-- Update the 'DescriptionAlternate' attribute to large text & Insert new attribute FutureDateAllowed
 
 UPDATE "DisplayProperties"
 SET "Value" = 'large text'
-WHERE "PropertyName" = 'DescriptionAlternate' AND "Attribute" = 'ComponentType';
+WHERE "PropertyName" = 'DescriptionAlternate'
+  AND "Attribute" = 'ComponentType';
+
+INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
+VALUES ('FoiExemptionAsserted', 'FutureDateAllowed', false, 'boolean');
+
+INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
+VALUES ('ClosureStartDate', 'FutureDateAllowed', true, 'boolean');
+
+INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
+VALUES ('start_date', 'FutureDateAllowed', true, 'boolean');
+
+INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
+VALUES ('end_date', 'FutureDateAllowed', true, 'boolean');
+
+INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
+VALUES ('date_created', 'FutureDateAllowed', true, 'boolean');
 
 COMMIT;
