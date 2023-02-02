d617c222a99a9115827482a1788cc4be0cdc0e76
thanhz <thanhz@hotmail.com>
2023-02-02 13:46:34 +0000

    No longer need futureDate inserts

--- lambda/src/main/resources/db/migration/V99__update_displayproperties.sql
+++ lambda/src/main/resources/db/migration/V99__update_displayproperties.sql
@@ -1,22 +1,9 @@
 -- Update the 'DescriptionAlternate' attribute to large text
--- Insert new attribute AllowFutureDate
 -- Delete 'date_created' from displayProperties
 
 UPDATE "DisplayProperties"
 SET "Value" = 'large text'
 WHERE "PropertyName" = 'DescriptionAlternate' AND "Attribute" = 'ComponentType';
-
-INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
-VALUES ('FoiExemptionAsserted', 'AllowFutureDate', false, 'boolean');
-
-INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
-VALUES ('ClosureStartDate', 'AllowFutureDate', false, 'boolean');
-
-INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
-VALUES ('start_date', 'AllowFutureDate', false, 'boolean');
-
-INSERT INTO "DisplayProperties" ("PropertyName", "Attribute", "Value", "AttributeType")
-VALUES ('end_date', 'AllowFutureDate', false, 'boolean');
 
 DELETE FROM "DisplayProperties" WHERE "PropertyName" = 'date_created';
 
