95faf1a969b1198f4e71828cda31bca368bddf38
Paul Young <paul.young@nationalarchives.gov.uk>
2023-05-22 10:11:42 +0100

    added a space between each column name for index V119

--- lambda/src/main/resources/db/migration/V119__getConsignment_further_indexes.sql
+++ lambda/src/main/resources/db/migration/V119__getConsignment_further_indexes.sql
@@ -2,14 +2,14 @@
 
 CREATE INDEX IF NOT EXISTS file_allfields_idx 
 ON public."File" 
-USING btree ("ConsignmentId", "FileId", "FileType","FileName","ParentId","UserId","Datetime","ChecksumMatches");
+USING btree ("ConsignmentId", "FileId", "FileType", "FileName", "ParentId", "UserId", "Datetime", "ChecksumMatches");
 
 CREATE INDEX IF NOT EXISTS filemetadata_allfields_idx 
 ON public."FileMetadata" 
-USING btree ("FileId", "PropertyName","Value","MetadataId", "Datetime","UserId");
+USING btree ("FileId", "PropertyName", "Value", "MetadataId", "Datetime", "UserId");
 
 CREATE INDEX IF NOT EXISTS filestatus_allfields_idx 
 ON public."FileStatus" 
-USING btree ("FileId", "StatusType","Value","FileStatusId", "CreatedDatetime");
+USING btree ("FileId", "StatusType", "Value", "FileStatusId", "CreatedDatetime");
 
 COMMIT;
