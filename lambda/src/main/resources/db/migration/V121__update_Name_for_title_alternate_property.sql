bea4d84032c53acf2dc45303dd38a6415933d904
vimleshg <vimlesh.gupta@nationalarchives.gov.uk>
2023-07-07 08:42:17 +0100

    Pull request feedback

--- lambda/src/main/resources/db/migration/V121__update_Name_for_title_alternate_property.sql
+++ lambda/src/main/resources/db/migration/V121__update_Name_for_title_alternate_property.sql
@@ -1,9 +1,7 @@
--- Update the displayProperties
-
+-- Update Name for the 'TitleAlternate' property
 
 UPDATE "DisplayProperties"
 SET "Value" = 'Add alternative title without the file extension'
 WHERE "PropertyName" = 'TitleAlternate' AND "Attribute" = 'Name';
-
 
 COMMIT;
