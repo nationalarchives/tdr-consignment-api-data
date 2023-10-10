70f6536cec4eda5a71446159094da006f329a560
vimleshg <vimlesh.gupta@nationalarchives.gov.uk>
2023-10-10 16:17:24 +0100

    Backfill 'SeriesName' and 'TransferringBodyName' only if they are empty

--- lambda/src/main/resources/db/migration/V135__backfill_new_columns_of_consignment.sql
+++ lambda/src/main/resources/db/migration/V135__backfill_new_columns_of_consignment.sql
@@ -3,6 +3,6 @@
 UPDATE "Consignment" c
 SET "SeriesName" =  s."Name", "TransferringBodyName" = b."Name"
 FROM "Series" s, "Body" b
-WHERE c."SeriesId" = s."SeriesId" and c."BodyId" = b."BodyId";
+WHERE coalesce(c."SeriesName", '') = '' and coalesce(c."TransferringBodyName", '') = '' and c."SeriesId" = s."SeriesId" and c."BodyId" = b."BodyId";
 
 COMMIT;
