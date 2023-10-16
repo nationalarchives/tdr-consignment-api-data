54c1d4644f687ce200f146f7b9d21ba7de083aea
Thanh <tri.lam21@outlook.com>
2023-10-16 12:55:44 +0100

    Add 'ParentReference attribute' + change order of 'FileReference'

---
+++ lambda/src/main/resources/db/migration/V138__add_parentreference_property.sql
@@ -0,0 +1,17 @@
+-- Add ParentReference property to File table
+ALTER TABLE "File"
+    ADD COLUMN "ParentReference" text;
+
+-- Move order of FileReference in the fileProperty table
+
+UPDATE "FileProperty"
+SET "ExportOrdinal" = 1
+WHERE "Name" = 'FileReference';
+
+-- Add ParentReference property
+
+INSERT INTO "FileProperty" ("Name", "Description", "FullName", "PropertyType", "Datatype", "Editable", "MultiValue",
+                            "PropertyGroup", "AllowExport", "ExportOrdinal")
+VALUES ('ParentReference', NULL, 'parent_reference', 'System', 'text', false, false, 'System', true, 137);
+
+COMMIT;
