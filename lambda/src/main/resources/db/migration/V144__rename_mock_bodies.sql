cd09598065ee153d4745689430d2e1179a194b8a
TomJKing <tom.king@nationalarchives.gov.uk>
2024-01-15 13:59:02 +0000

    Pull request feedback

--- lambda/src/main/resources/db/migration/V144__rename_mock_bodies.sql
+++ lambda/src/main/resources/db/migration/V144__rename_mock_bodies.sql
@@ -1,11 +1,11 @@
 -- In the Body table, change "Name" values
--- MOCK1 --> MOCK 1
--- MOCK2 --> MOCK 2
--- MOCK3 --> MOCK 3
+-- MOCK1 --> Mock 1
+-- MOCK2 --> Mock 2
+-- MOCK3 --> Mock 3
 -- (There are three affected rows).
 
-UPDATE public."Body" SET "Name" = 'MOCK 1 Department' WHERE "Name" = 'MOCK1 Department';
-UPDATE public."Body" SET "Name" = 'MOCK 2 Department' WHERE "Name" = 'MOCK2 Department';
-UPDATE public."Body" SET "Name" = 'MOCK 3 Department' WHERE "Name" = 'MOCK3 Department';
+UPDATE public."Body" SET "Name" = 'Mock 1 Department' WHERE "Name" = 'MOCK1 Department';
+UPDATE public."Body" SET "Name" = 'Mock 2 Department' WHERE "Name" = 'MOCK2 Department';
+UPDATE public."Body" SET "Name" = 'Mock 3 Department' WHERE "Name" = 'MOCK3 Department';
 
 COMMIT;
