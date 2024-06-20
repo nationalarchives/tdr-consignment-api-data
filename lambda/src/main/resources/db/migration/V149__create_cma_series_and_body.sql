00044cfc07cd5899172374ab05fd6468073a43ef
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-06-20 12:43:33 +0100

    Correct abbreviation `CMA`

--- lambda/src/main/resources/db/migration/V149__create_cma_series_and_body.sql
+++ lambda/src/main/resources/db/migration/V149__create_cma_series_and_body.sql
@@ -4,7 +4,7 @@ DO $$
     BEGIN
         -- Insert CMA body into the  Body Table
         INSERT INTO "Body" ("BodyId", "Name", "Description", "TdrCode") VALUES
-            (uuid_generate_v4(), 'Competition and Markets Authority (Cma)', 'Competition and Markets Authority (Cma)', 'TDR-CMA') RETURNING "BodyId" INTO bodyUuid;
+            (uuid_generate_v4(), 'Competition and Markets Authority (CMA)', 'Competition and Markets Authority (CMA)', 'TDR-CMA') RETURNING "BodyId" INTO bodyUuid;
 
         -- Use the returned bodyUuid value for the series insert
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
