6a3b904a6e2693849cddf4c49a118454f3974da5
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-07-09 11:11:48 +0100

    Add series EC 13

--- lambda/src/main/resources/db/migration/V150__add_ec13_series.sql
+++ lambda/src/main/resources/db/migration/V150__add_ec13_series.sql
@@ -1,9 +1,9 @@
 DO $$
     DECLARE
-        -- Retrieve Welsh Government body uuid
+        -- Retrieve Electoral body uuid
         EcBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-EC');
     BEGIN
-        -- Use the returned WaBodyUuid value for the series insert
+        -- Use the returned EcBodyUuid value for the series insert
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
             (uuid_generate_v4(), EcBodyUuid, 'EC 13', 'EC 13', 'EC 13');
     END $$;
