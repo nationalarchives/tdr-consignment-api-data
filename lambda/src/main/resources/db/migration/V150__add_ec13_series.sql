34c35abb513e5400e2afaa5aefae050c0d29f7a1
ian-hoyle <ian.hoyle.tna@gmail.com>
2024-07-09 12:14:14 +0100

    Add series EC 13 (#379)
    * Add series EC 13

---
+++ lambda/src/main/resources/db/migration/V150__add_ec13_series.sql
@@ -0,0 +1,12 @@
+DO $$
+    DECLARE
+        -- Retrieve Electoral Commission body uuid
+        EcBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-EC');
+    BEGIN
+        -- Use the returned EcBodyUuid value for the series insert
+        INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
+            (uuid_generate_v4(), EcBodyUuid, 'EC 13', 'EC 13', 'EC 13');
+    END $$;
+
+--commit changes
+COMMIT;
