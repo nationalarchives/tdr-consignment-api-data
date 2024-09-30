2fe1e8989134aacfecf78b4dde2c5e9b84d3ece9
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2024-09-30 11:40:27 +0100

    Update variable name

--- lambda/src/main/resources/db/migration/V153__add_series_for_HMCTS.sql
+++ lambda/src/main/resources/db/migration/V153__add_series_for_HMCTS.sql
@@ -5,7 +5,7 @@ DO $$
     BEGIN
         -- Use the returned HmctsBodyUuid value to insert new series
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
-            (uuid_generate_v4(), EcBodyUuid, 'LE 11', 'LE 11', 'LE 11');
+            (uuid_generate_v4(), HmctsBodyUuid, 'LE 11', 'LE 11', 'LE 11');
     END $$;
 
 --commit changes
