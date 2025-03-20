a40521a3cb20da6e9e15458c4ff34b8da2ab8907
ian-hoyle <ian.hoyle.tna@gmail.com>
2025-03-20 15:19:37 +0000

    Update V162__add_jx11_series.sql

--- lambda/src/main/resources/db/migration/V162__add_jx11_series.sql
+++ lambda/src/main/resources/db/migration/V162__add_jx11_series.sql
@@ -3,7 +3,7 @@ DO $$
         -- Retrieve ICO body uuid
         icoBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-ICO');
     BEGIN
-        -- Use the returned NiceBodyUuid value for the series insert
+        -- Use the returned icoBodyUuid value for the series insert
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
             (uuid_generate_v4(), icoBodyUuid, 'JX 11', 'JX 11', 'JX 11');
     END $$;
