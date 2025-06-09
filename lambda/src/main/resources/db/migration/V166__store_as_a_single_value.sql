ee263a63a2989fe9536e7cf981944640291f32d4
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2025-06-10 00:54:24 +0100

    TDRD-844 - Combine values for multi value properties

---
+++ lambda/src/main/resources/db/migration/V166__store_as_a_single_value.sql
@@ -0,0 +1,82 @@
+-- Combine values for multi value properties in FileMetadata table
+DO $$
+
+    BEGIN
+
+        -- Process FoiExemptionCode property
+        WITH
+        multi_foi_exemption_codes AS (SELECT "FileId"
+                                      FROM "FileMetadata"
+                                      WHERE "PropertyName" = 'FoiExemptionCode'
+                                      GROUP BY "FileId"
+                                      HAVING COUNT(*) > 1),
+
+        -- Combine the FoiExemptionCode values
+        combine_foi_exemption_codes AS (SELECT "FileId",
+                                               "UserId",
+                                               STRING_AGG("Value", ';' ORDER BY "Value") AS combined_value,
+                                               MAX("Datetime")                           AS latest_datetime
+                                        FROM "FileMetadata"
+                                        WHERE "PropertyName" = 'FoiExemptionCode'
+                                          AND "FileId" IN (SELECT "FileId" FROM multi_foi_exemption_codes)
+                                        GROUP BY "FileId", "UserId"),
+
+        -- Delete the old FoiExemptionCode rows
+        deleted_exemptions AS (
+            DELETE FROM "FileMetadata"
+                WHERE "PropertyName" = 'FoiExemptionCode'
+                    AND "FileId" IN (SELECT "FileId" FROM multi_foi_exemption_codes)
+                RETURNING "FileId")
+
+        -- Insert new FoiExemptionCode rows with combined values
+                INSERT
+                INTO "FileMetadata" ("MetadataId", "FileId", "Value", "Datetime", "UserId", "PropertyName")
+                SELECT uuid_generate_v4(),
+                       codes."FileId",
+                       codes.combined_value,
+                       codes.latest_datetime,
+                       CAST(codes."UserId" AS text)::uuid,
+                       'FoiExemptionCode'
+                FROM combine_foi_exemption_codes codes;
+
+        -- Process Language property
+        WITH
+        -- Get FileIds with multiple Language properties
+        multi_languages AS (SELECT "FileId"
+                            FROM "FileMetadata"
+                            WHERE "PropertyName" = 'Language'
+                            GROUP BY "FileId"
+                            HAVING COUNT(*) > 1),
+
+        -- Combine the Language values for these files
+        combine_languages AS (SELECT "FileId",
+                                     "UserId",
+                                     STRING_AGG("Value", ';' ORDER BY "Value") AS combined_value,
+                                     MAX("Datetime")                           AS latest_datetime
+                              FROM "FileMetadata"
+                              WHERE "PropertyName" = 'Language'
+                                AND "FileId" IN (SELECT "FileId" FROM multi_languages)
+                              GROUP BY "FileId", "UserId"),
+
+        -- Delete the old Language rows
+        deleted_rows AS (
+            DELETE FROM "FileMetadata"
+                WHERE "PropertyName" = 'Language'
+                    AND "FileId" IN (SELECT "FileId" FROM multi_languages)
+                RETURNING "FileId")
+
+        -- Insert new Language rows with combined values
+                INSERT
+                INTO "FileMetadata" ("MetadataId", "FileId", "Value", "Datetime", "UserId", "PropertyName")
+                SELECT uuid_generate_v4(),
+                       cl."FileId",
+                       cl.combined_value,
+                       cl.latest_datetime,
+                       CAST(cl."UserId" AS text)::uuid,
+                       'Language'
+                FROM combine_languages cl;
+
+    END $$;
+
+--commit changes
+COMMIT;
