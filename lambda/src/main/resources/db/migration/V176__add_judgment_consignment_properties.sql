bac4aea18bdf4b3e298768c009d80e7914aef0a7
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-08-28 14:16:05 +0100

    add ConsignmentProperties for judgment

---
+++ lambda/src/main/resources/db/migration/V176__add_judgment_consignment_properties.sql
@@ -0,0 +1,8 @@
+INSERT INTO "ConsignmentProperty" ("Name", "Description")
+VALUES ('JudgmentType', '' ),
+       ('JudgmentUpdate', ''),
+       ('JudgmentUpdateType', ''),
+       ('JudgmentUpdateDetails', ''),
+       ('JudgmentNeutralCitation', ''),
+       ('JudgmentNoNeutralCitation', ''),
+       ('JudgmentReference', '')
