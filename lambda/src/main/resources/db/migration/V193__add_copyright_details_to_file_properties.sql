fae52ef027a050c91de798b70c368b8a157f2dae
ian-hoyle <ian.hoyle.tna@gmail.com>
2026-02-12 13:21:43 +0000

    CopyrightDetails (#617)

---
+++ lambda/src/main/resources/db/migration/V193__add_copyright_details_to_file_properties.sql
@@ -0,0 +1,6 @@
+-- Add CopyrightDetails property
+
+INSERT INTO "FileProperty" ("Name", "Description", "FullName", "Datatype")
+VALUES ('CopyrightDetails', NULL, 'copyright_details', 'text');
+
+COMMIT;
