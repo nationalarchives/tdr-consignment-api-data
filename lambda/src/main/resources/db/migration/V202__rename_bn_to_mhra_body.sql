1e14b75fd5333a0794b4c409f18b46ec1dfedd23
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-04-24 09:49:46 +0100

    TDRD-1418 - Rename body from TDR-BN to TDR-MHRA

---
+++ lambda/src/main/resources/db/migration/V202__rename_bn_to_mhra_body.sql
@@ -0,0 +1,5 @@
+UPDATE "Body"
+SET "Name"        = 'Medicines and Healthcare products Regulatory Agency',
+    "Description" = 'Medicines and Healthcare products Regulatory Agency',
+    "TdrCode"     = 'TDR-MHRA'
+WHERE "TdrCode" = 'TDR-BN';
