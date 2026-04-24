c112f0466658e7de5cefedce181103700b8a1666
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-04-24 11:05:52 +0100

    PR feedback

--- lambda/src/main/resources/db/migration/V202__delete_bn_body_and_add_series_to_mhra_body.sql
+++ lambda/src/main/resources/db/migration/V202__delete_bn_body_and_add_series_to_mhra_body.sql
@@ -1,18 +1,12 @@
--- Remove BN 116 series and TDR-BN body as BN 116 series is now being added to TDR-MHRA body instead of TDR-BN body.
-DELETE from "Series"
-WHERE "BodyId" IN (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-BN');
-DELETE from "Body" where "TdrCode" = 'TDR-BN';
+-- Delete BN body and update BN 116 series to point to MHRA body
 
--- Add BN 116 series for TDR-MHRA
-DO $$
-    DECLARE
-        -- Retrieve MHRA body uuid
-        MHRABodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-MHRA');
-    BEGIN
-        -- Use the returned MHRABodyUuid value for the series insert
-        INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
-            (uuid_generate_v4(), MHRABodyUuid, 'BN 116', 'BN 116', 'BN 116');
-    END $$;
+UPDATE "Series"
+SET "BodyId" = (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-MHRA')
+WHERE "Code" = 'BN 116';
+
+DELETE
+from "Body"
+where "TdrCode" = 'TDR-BN';
 
 --commit changes
 COMMIT;
