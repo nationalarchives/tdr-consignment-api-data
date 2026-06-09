c307365306ca78e7683aff9987df616fab120a47
ian-hoyle <ian.hoyle.tna@gmail.com>
2026-06-09 11:09:11 +0100

    Fix comment for series insert in migration script
    Updated comment to reflect the correct variable name used for the series insert.

--- lambda/src/main/resources/db/migration/V206__add_ec14_series.sql
+++ lambda/src/main/resources/db/migration/V206__add_ec14_series.sql
@@ -3,7 +3,7 @@ DO $$
         -- Retrieve EC body uuid
        EcBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-EC');
     BEGIN
-        -- Use the returned NiceBodyUuid value for the series insert
+         -- Use the returned EcBodyUuid value to insert new series
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
             (uuid_generate_v4(), EcBodyUuid, 'EC 14', 'EC 14', 'EC 14');
     END $$;
