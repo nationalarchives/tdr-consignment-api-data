ea388782bbf5df1e5598c1cab5b25356eb5404e2
TomJKing <tom.king@nationalarchives.gov.uk>
2026-06-12 08:49:54 +0100

    Remove duplicate series

--- lambda/src/main/resources/db/migration/V207__create_hmt_body_and_series.sql
+++ lambda/src/main/resources/db/migration/V207__create_hmt_body_and_series.sql
@@ -10,7 +10,6 @@ DO $$
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
             (uuid_generate_v4(), bodyUuid, 'T725', 'T725', 'T725'),
             (uuid_generate_v4(), bodyUuid, 'T729', 'T729', 'T729'),
-            (uuid_generate_v4(), bodyUuid, 'T733', 'T733', 'T733'),
             (uuid_generate_v4(), bodyUuid, 'T731', 'T731', 'T731'),
             (uuid_generate_v4(), bodyUuid, 'T733', 'T733', 'T733'),
             (uuid_generate_v4(), bodyUuid, 'T728', 'T728', 'T728');
