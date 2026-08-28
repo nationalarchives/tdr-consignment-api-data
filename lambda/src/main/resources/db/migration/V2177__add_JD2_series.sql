783beda1c1f94770d1ad014fd1a8b1f343066e15
ttlam <thanh.lam@nationalarchives.gov.uk>
2026-08-28 10:40:55 +0100

    slight rewords

--- lambda/src/main/resources/db/migration/V2177__add_JD2_series.sql
+++ lambda/src/main/resources/db/migration/V2177__add_JD2_series.sql
@@ -1,11 +1,11 @@
 DO $$
     DECLARE
         -- Retrieve CMA body uuid
-       HmtBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-CMA');
+       CmaBodyUuid "Body"."BodyId"%TYPE := (SELECT "BodyId" FROM "Body" WHERE "TdrCode" = 'TDR-CMA');
     BEGIN
-         -- Use the returned HmtBodyUuid value to insert new series
+         -- Use the returned CmaBodyUuid value to insert new series
         INSERT INTO "Series" ("SeriesId", "BodyId", "Code", "Name", "Description") VALUES
-            (uuid_generate_v4(), HmtBodyUuid, 'JD 2', 'JD 2', 'JD 2');
+            (uuid_generate_v4(), CmaBodyUuid, 'JD 2', 'JD 2', 'JD 2');
     END $$;
 
 --commit changes
