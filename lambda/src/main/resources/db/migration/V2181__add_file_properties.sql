90f141a18bd9fb3cdf8e86654cfbf894e058bffc
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-09-22 10:01:12 +0100

    PR feedback

--- lambda/src/main/resources/db/migration/V2181__add_file_properties.sql
+++ lambda/src/main/resources/db/migration/V2181__add_file_properties.sql
@@ -1,8 +1,7 @@
 -- Add following properties to FileProperty table: CataloguePlacement, CitableRefPrefix, Inventor, FormerFilepathDepartment
 
-
 INSERT INTO "FileProperty" ("Name", "FullName", "Datatype")
-VALUES ('CataloguePlacement', 'CataloguePlacement', 'text'),
-	   ('CitableRefPrefix', 'CitableRefPrefix', 'text'),
-	   ('Inventor', 'Inventor', 'text'),
-	   ('FormerFilepathDepartment', 'FormerFilepathDepartment', 'text');
+VALUES ('CataloguePlacement', 'catalogue_placement', 'text'),
+	   ('CitableRefPrefix', 'citable_ref_prefix', 'text'),
+	   ('Inventor', 'inventor', 'text'),
+	   ('FormerFilepathDepartment', 'former_filepath_department', 'text');
